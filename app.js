a93e3fd6d1beb5f5f40ff999b70bc306d1c5d2db
Vaibhav Chopra <vaibhavchopra@Vaibhavs-MacBook-Air.local>
2020-07-12 00:01:21 +0530

    🌺 initial commit

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
+var db = firebase.firestore();
 
+db.collection("cafes").onSnapshot((snapshot) => {
+  let list = ``;
+  snapshot.forEach((doc) => {
+    const docData = doc.data();
+    list += `<li>
+            <span>${docData.name}</span>
+            <span>${docData.city}</span>
+            <div onClick="deleteCafe('${doc.id}')">x</div>
+        </li>`;
+  });
+  document.querySelector("#cafe-list").innerHTML = list;
+});
+
+const addForm = document.querySelector("#add-cafe-form");
+addForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  db.collection("cafes")
+    .add({
+      name: addForm["name"].value,
+      city: addForm["city"].value,
+    })
+    .then(() => {
+      addForm.reset();
+    });
+});
+
+function deleteCafe(docId) {
+  db.collection("cafes")
+    .doc(docId)
+    .delete()
+    .then(() => {});
+}
--- config.js
+++ config.js
@@ -1,1 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDtEw2tNrSQATILIgNzfqJhlYm_Eu9lUgI",
+  authDomain: "to-do-app-6b600.firebaseapp.com",
+  databaseURL: "https://to-do-app-6b600.firebaseio.com",
+  projectId: "to-do-app-6b600",
+  storageBucket: "to-do-app-6b600.appspot.com",
+  messagingSenderId: "385214361553",
+  appId: "1:385214361553:web:3c497f66e9d6c095333af0",
+};
+// Initialize Firebase
 
+firebase.initializeApp(firebaseConfig);
